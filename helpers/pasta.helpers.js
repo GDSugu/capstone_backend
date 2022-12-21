6b760cbdf29e9f5d4424ba744ade2dd6f3c59492
Sugumar Srinivasan <94180748+GDSugu@users.noreply.github.com>
2022-12-21 20:02:03 +0530

    Update pasta.helpers.js

--- helpers/pasta.helpers.js
+++ helpers/pasta.helpers.js
@@ -3,7 +3,7 @@ const db = require("../shared/mongo");
 
 module.exports = {
     findAll(){
-        return db.pasta.find().toArray();
+        return db.pasta.find().limt(20).toArray();
     },
 
     findById(_id){
