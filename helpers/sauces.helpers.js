155109b69ce92d3f8e24870fb1c4c4f754d8d268
Sugumar Srinivasan <94180748+GDSugu@users.noreply.github.com>
2022-12-27 20:12:41 +0530

    Update sauces.helpers.js

--- helpers/sauces.helpers.js
+++ helpers/sauces.helpers.js
@@ -3,7 +3,7 @@ const db = require("../shared/mongo");
 
 module.exports = {
     findAll(){
-        return db.sauces.find().limt(20).toArray();
+        return db.sauces.find().toArray();
     },
 
 
